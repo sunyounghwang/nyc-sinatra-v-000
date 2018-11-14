93205316a9928d594b7873376ffc2d315ea335dd
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-11-14 17:18:56 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,36 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  helpers do
+    def all_landmarks
+      Landmark.all
+    end
+  end
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,22 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :figure_titles do |t|
+      t.integer :figure_id
+      t.integer :title_id
+    end
+  end
 end
